215a8415b7c60ba90879245e0b142e9930530360
jamesnem <jenemecek@deakin.edu.au>
2021-12-15 00:52:05 +1100

    add database and carousel

--- public/env.js
+++ public/env.js
@@ -1,24 +1,89 @@
-const testButtonFunction=()=>{
-  alert('Thank you for clicking')
-}
-
 // connect to the socket
-
 let socket = io();
 
 socket.on('number', (msg) => {
-    console.log('Random number: ' + msg);
+  console.log('Random number: ' + msg);
 })
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
-});
-$(document).ready(function(){
-  console.log('Ready')
+function contentsCards(proj){
+  if (proj.projectID == 1){
+    proj.title = "SubLearn";
+    proj.img = "assets/sublearn.png";
+    proj.info = "Data driven android application to help learner drivers select safer suburbs to practice driving.";
+  } else if (proj.projectID == 2){
+    proj.title = "Lockdown Explorer";
+    proj.img = "assets/explorer.png";
+    proj.info = "Mobile application that encourages social connectedness through activeness";
+  } else if (proj.projectID == 3){
+    proj.title = "C4Net Analyses";
+    proj.img = "assets/c4net.png";
+    proj.info = "Performed visual analysis on Powercor and Jemena energy datasets.";
+  }
+}
+
+function projectDisplay(project) {
+  return `
+  <a class="carousel-item black-text"">
+    <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="${project.img ? project.img : "assets/no_image.png"}">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">${project.title}<i class="material-icons right">more_vert</i></span>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">${project.title}<i class="material-icons right">close</i></span>
+        <p>${project.info}</p>
+      </div>
+    </div>
+  </a>`;
   
-  $("#main-nav").load("components/navbar.html",()=> {
+                     
+}
+
+
+$(document).ready(function () {
+
+  $("#main-nav").load("components/navbar.html", () => {
     $('.sidenav').sidenav();
-   
   })
+
+  $.get('/projects', (result) => {
+    for (let items of result) {
+      contentsCards(items)
+      $('#carousel-projects').append(projectDisplay(items))
+      console.log(items)
+    }
+    $('#carousel-projects').carousel();
+  })
+
+  $('.modal').modal();
+
+  $("#submit-project").click((e) => {
+    const data = {
+        projectID: $("#project-id").val(),
+        title: $("#project-title").val(),
+        info: $("#project-description").val(),
+        img: null
+    };
+
+    var settings = {
+      "url": "/projects",
+      "method": "POST",
+      "timeout":0,
+      "headers": {
+        "Content-type": "application/json"
+      },
+      "data": JSON.stringify(data)
+    };
+
+    $.ajax(settings).done(function(response) {
+      contentsCards(data)
+      $('#carousel-projects').append(projectDisplay(data))
+      console.log(response)
+    })
+
+    alert("Project Submitted")
+  });
+    
 })
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 let express = require("express");
+let dbCon = require("./db/connection");
+var cors = require('cors');
 let app = express();
 
 //var app = require('express')();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
-
-
-
-
+const bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(cors());
 
 app.get("/test", function (request, response) {
-  var user_name = request.query.user_name;
+  var user_name = "James Nemecek";
   response.end("Hello " + user_name + "!");
 });
-
 
 //socket test
 io.on('connection', (socket) => {
@@ -25,16 +25,54 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  setInterval(()=>{
-    socket.emit('number', parseInt(Math.random()*10));
+  setInterval(() => {
+    socket.emit('number', parseInt(Math.random() * 10));
   }, 1000);
+});
 
+const projects = [];
+for (let id = 1; id < 6; id++) {
+  projects.push({
+    projectID: id,
+    title: `Project ${id}`,
+    info: `Information about ${id}`,
+    img: null,
+  });
+}
+
+app.get("/projects", function (request, response) {
+  //response.json(projects);
+  dbCon.getDb().collection("projectData").find({}).toArray((err, res) =>{
+    if (err)
+      throw err
+    response.send(res);
+  });
+});
+
+app.post("/projects", function (request, response) {
+  const project = request.body;
+  console.log(JSON.stringify(project));
+  if (project) {
+    dbCon.getDb().collection("projectData").insertOne(project)
+  } else {
+    response.sendStatus(500);
+  }
+  response.sendStatus(204);
 });
 
 
-http.listen(port,()=>{
-  console.log("Listening on port ", port);
+
+dbCon.connectToDatabase(function (err) {
+
+  if (err) {
+    console.error(err);
+    process.exit();
+  }
+  http.listen(port,() => {
+    console.log("Listening on port ", port);
+  });
 });
+
 
 //this is only needed for Cloud foundry 
 require("cf-deployment-tracker-client").track();
